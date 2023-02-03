6512f4fb43e3bbbd7ce0b1847ace135e3e8462f7
omgismartinez <martnzomg@gmail.com>
2023-02-03 01:18:18 -0600

    🌌 refactor (twitch): change of the library 'comfy' to 'tmi' and refactor functions

--- src/utils/twitch.ts
+++ src/utils/twitch.ts
@@ -1,30 +1,43 @@
-import comfy from 'comfy.js'
+import { ChatProps } from '@/types/tmi'
+import tmi from 'tmi.js'
+
+let client: tmi.Client | null = null
 
 export const connect = async (username: string) => {
     try {
-        await new Promise<void>((resolve) => {
-            comfy.Init(username)
-            onConnected(() => resolve())
+        client = new tmi.Client({
+            connection: {
+                reconnect: true,
+                secure: true,
+            },
+            channels: [username],
         })
+
+        return await client.connect()
+            .then(() => console.log(`Connected to ${username}`))
     } catch {
         console.log('Error connecting')
     }
 }
 
-export const onConnected = (callback: (address: string, port: number) => void) => {
-    comfy.onConnected = callback
-}
-
-export const onChat = (callback: (user: string, message: string, flags: any, self: boolean) => void) => {
-    comfy.onChat = callback
+export const onChat = (callback: (props: ChatProps) => void) => {
+    client?.on('chat', (channel, userstate, message, self) => callback({
+        channel,
+        userstate,
+        message,
+        self,
+    }))
 }
 
 export const disconnect = async () => {
     try {
-        await new Promise<void>((resolve) => {
-            resolve(comfy.Disconnect())
-        }).then(() => console.log('Disconnected'))
+        return await client?.disconnect()
+            .then(() => console.log('Disconnected'))
     } catch {
         console.log('Error disconnecting')
     }
+}
+
+export const removeListeners = () => {
+    client?.removeAllListeners()
 }

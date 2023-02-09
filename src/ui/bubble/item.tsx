9e5e3391a83c231279df3a8b68433f06d1f2f5a5
omgismartinez <martnzomg@gmail.com>
2023-02-09 01:56:41 -0600

    🌌 refactor (item): borderRadius dynamic

--- src/ui/bubble/item.tsx
+++ src/ui/bubble/item.tsx
@@ -8,18 +8,15 @@ export function Item({ row, col, planeX, planeY }: { row: number, col: number, p
     const x = useMotionValue(0)
     const y = useMotionValue(0)
     const scale = useMotionValue(1)
+    const borderRadius = useMotionValue(16)
 
     // Calculate the origin x and y offsets of this icon based on 
     // its column and row position
     const xOffset = col * (icon.size + icon.margin) + (row % 2) * ((icon.size + icon.margin) / 2);
     const yOffset = row * icon.size;
 
-    // const xOffset = col * (icon.size + icon.margin)
-    // const yOffset = row * icon.size + (row * icon.margin)
-
     // Transform the icon's x, y and scale based on the position of the draggable plane
-    useIconTransform({ x, y, scale, planeX, planeY, xOffset, yOffset })
-
+    useIconTransform({ x, y, scale, planeX, planeY, xOffset, yOffset, borderRadius })
     return (
         <motion.div
             style={{
@@ -31,8 +28,7 @@ export function Item({ row, col, planeX, planeY }: { row: number, col: number, p
                 scale,
                 width: icon.size,
                 height: icon.size,
-                // borderRadius: 16,
-                borderRadius: '50%',
+                borderRadius,
                 // This will change the color of an icon every render. In production
                 // you'd want to save this as a ref or similar. But here it makes a nice
                 // visual indicator that we're doing all this without any re-renders :)
